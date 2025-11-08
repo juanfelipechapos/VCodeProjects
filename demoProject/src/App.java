bc966c016832ef82df055a7c178c867e80b22bf5
jfelipechapos <juanfelipe2217@gmail.com>
2025-11-08 12:16:41 -0600

    test 3 again

--- demoProject/src/App.java
+++ demoProject/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!, just testing CI/CD pipeline.");
+        System.out.println("Hello, World!, just testing CI/CD pipeline, again");
     }
 }
